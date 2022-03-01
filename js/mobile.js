5cd042d1d1103f4a7532bfccf665a5c5633daa6e
mehedi hasan <mehedicsebu@gmail.com>
2022-03-01 19:57:53 +0600

    error modified

--- js/mobile.js
+++ js/mobile.js
@@ -1,41 +1,36 @@
 const searchPhone = () => {
     const searchInput = document.getElementById('input-search');
     const searchValue = searchInput.value;
-    // remove input value
-    if (searchInput.value == '') {
-        const blank = document.getElementById('blank');
-        blank.style.display = 'block';
-    }
-    else {
-        //remove no input error
-        const blank = document.getElementById('blank');
-        blank.style.display = 'none';
-
-        const url = `
+    searchInput.value = '';
+    const url = `
     https://openapi.programming-hero.com/api/phones?search=${searchValue}
     `;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displaySearch(data.data))
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearch(data.data))
+}
+const displaySearch = (data) => {
+    //console.log(data);
+    const searchResult = document.getElementById('search-result');
+    //previous phone item remove
+    searchResult.textContent = '';
+
+    //if no result found
+    if (data == 0) {
+        const showText = document.getElementById('result');
+        showText.style.display = 'block';
     }
-    const displaySearch = (data) => {
-        //console.log(data);
-        const searchResult = document.getElementById('search-result');
-        //previous phone item remove
-        searchResult.textContent = '';
+    else {
+        //Remove no found result
+        const showText = document.getElementById('result');
+        showText.style.display = 'none';
+    }
 
-        //if no result found
-        if (data == 0) {
-            const showText = document.getElementById('result');
-            showText.style.display = 'block';
-        }
-        //showText.innerText = '';
-
-        data.forEach(mobile => {
-            const div = document.createElement('div');
-            div.classList.add('col');
-            //console.log(mobile.slug)
-            div.innerHTML = `
+    data.forEach(mobile => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        //console.log(mobile.slug)
+        div.innerHTML = `
         <div class="card m-3">
                 <img height="400px"  src="${mobile.image}" class="card-img-top" alt="image">
                 <div class="card-body m-2">
@@ -44,35 +39,32 @@ const searchPhone = () => {
                     <button onclick="showDetails('${mobile.slug}')" type="button" class="btn btn-secondary">Show Details</button>
                 </div>
         `;
-            searchResult.appendChild(div);
-        });
-    }
-
-    //Remove no found result
-    const showText = document.getElementById('result');
-    showText.style.display = 'none';
+        searchResult.appendChild(div);
+    });
+}
 
 
 
-    // Show details phone
-    const showDetails = phoneSlug => {
-        // console.log(phoneSlug);
-        const url = `https://openapi.programming-hero.com/api/phone/${phoneSlug}`;
-        // console.log(url)
-        fetch(url)
-            .then(response => response.json())
-            // .then(data => console.log(data))
-            .then(data => displayPhoneDetails(data))
-    }
 
-    //display single phone details
-    const displayPhoneDetails = (phones) => {
-        // console.log(phones);
-        const phoneDetail = document.getElementById('singlePhoneDetail');
-        const div = document.createElement('div');
-        div.classList.add('card', 'border', 'border-light');
-        // clear data
-        phoneDetail.innerHTML = `
+// Show details phone
+const showDetails = phoneSlug => {
+    // console.log(phoneSlug);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneSlug}`;
+    // console.log(url)
+    fetch(url)
+        .then(response => response.json())
+        // .then(data => console.log(data))
+        .then(data => displayPhoneDetails(data))
+}
+
+//display single phone details
+const displayPhoneDetails = (phones) => {
+    // console.log(phones);
+    const phoneDetail = document.getElementById('singlePhoneDetail');
+    const div = document.createElement('div');
+    div.classList.add('card', 'border', 'border-light');
+    // clear data
+    phoneDetail.innerHTML = `
     <img height="500px" src="${phones.data.image}" class="card-img-top" alt="...">
             <div class="card-body">
             <h4 class="card-title">${phones.data.brand}</h4>
@@ -91,7 +83,5 @@ const searchPhone = () => {
             <p class="card-title"><span class="fw-bold">USB:</span> ${phones.data.others.USB}</p>
             </div>
     `;
-        phoneDetail.appendChild(div);
-    }
-
+    phoneDetail.appendChild(div);
 }
