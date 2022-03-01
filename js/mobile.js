4104f31770434d6b5bd9f55e74d4095e8bc1212c
mehedi hasan <mehedicsebu@gmail.com>
2022-03-01 07:33:20 +0600

    search option added

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,14 @@
-console.log('script file is connected');
+const searchPhone = () => {
+    const searchInput = document.getElementById('input-search');
+    const searchValue = searchInput.value;
+    console.log(searchValue);
+    searchInput.value = '';
+
+    const url = `
+    https://openapi.programming-hero.com/api/phones?search=${searchValue}
+    `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    // console.log(url);
+}
