09bab518610e10e6fa6c5a298662a3081524b514
mehedi hasan <mehedicsebu@gmail.com>
2022-03-01 07:56:09 +0600

    phone show done

--- js/mobile.js
+++ js/mobile.js
@@ -9,6 +9,22 @@ const searchPhone = () => {
     `;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
-    // console.log(url);
+        .then(data => displaySearch(data.data))
+}
+const displaySearch = (data) => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+    data.forEach(mobile => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="showDetails(${mobile.brand})" class="card">
+                <img  src="${mobile.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${mobile.phone_name}</h5>
+                </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
